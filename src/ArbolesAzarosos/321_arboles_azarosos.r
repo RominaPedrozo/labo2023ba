e823e01d72b10b6f3c0507a95f45b42193779115
RominaPedrozo <rominapedrozoe@gmail.com>
2023-09-06 17:07:27 +0000

    1 321

--- src/ArbolesAzarosos/321_arboles_azarosos.r
+++ src/ArbolesAzarosos/321_arboles_azarosos.r
@@ -11,7 +11,7 @@ require("rpart")
 
 # parmatros experimento
 PARAM <- list()
-PARAM$experimento <- 3210
+PARAM$experimento <- 3211
 
 # Establezco la semilla aleatoria, cambiar por SU primer semilla
 PARAM$semilla <- 111422
@@ -19,14 +19,14 @@ PARAM$semilla <- 111422
 # parameetros rpart
 PARAM$rpart_param <- list(
   "cp" = -1,
-  "minsplit" = 250,
-  "minbucket" = 100,
-  "maxdepth" = 14
+  "minsplit" = 50,
+  "minbucket" = 10,
+  "maxdepth" = 8
 )
 
 # parametros  arbol
 # entreno cada arbol con solo 50% de las variables variables
-PARAM$feature_fraction <- 0.25
+PARAM$feature_fraction <- 0.5
 # voy a generar 500 arboles, a mas arboles mas tiempo de proceso y MEJOR MODELO
 #  pero ganancias marginales
 PARAM$num_trees_max <- 500
