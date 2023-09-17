4a0a7cd0e50bc44618604bf166a9a77494ad1d94
RominaPedrozo <rominapedrozoe@gmail.com>
2023-09-17 21:15:11 +0000

    Exp original 631

--- src/workflow-inicial/Exp_original_621_DR_corregir_drifting.r
+++ src/workflow-inicial/Exp_original_621_DR_corregir_drifting.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "DR6210"
+PARAM$experimento <- "DR6211"
 
-PARAM$exp_input <- "CA6110"
+PARAM$exp_input <- "CA6111"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
