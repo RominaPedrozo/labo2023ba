7853144c1b38040eb83eb11fc3bf45bff1f0ab74
RominaPedrozo <rominapedrozoe@gmail.com>
2023-10-04 12:57:41 +0000

    final 1

---
+++ src/workflow-inicial/Exp_FINAL_1_601_RUN_correr_workflow_01.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/Exp_original_611_CA_reparar_dataset_01.r")
+source("~/labo2023ba/src/workflow-inicial/Exp2_final_grupoA_621_DR_corregir_drifting.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_631_FE_historia.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_641_TS_training_strategy.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_1_651_HT_lightgbm.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_1_661_ZZ_final.r")
