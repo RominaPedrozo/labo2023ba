5c780dca7429742a96e84e8920cdd2b3826da9b9
RominaPedrozo <rominapedrozoe@gmail.com>
2023-10-04 15:25:44 +0000

    601 final 2

--- src/workflow-inicial/Exp_FINAL_2_601_RUN_correr_workflow_01.r
+++ src/workflow-inicial/Exp_FINAL_2_601_RUN_correr_workflow_01.r
@@ -13,8 +13,8 @@ options(error = function() {
 source("~/labo2023ba/src/workflow-inicial/Exp_original_611_CA_reparar_dataset_01.r")
 source("~/labo2023ba/src/workflow-inicial/Exp2_final_grupoA_621_DR_corregir_drifting.r")
 source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_631_FE_historia.r")
-source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_1_641_TS_training_strategy.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_2_641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_1_651_HT_lightgbm.r")
-source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_1_661_ZZ_final.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_2_651_HT_lightgbm.r")
+source("~/labo2023ba/src/workflow-inicial/Exp_FINAL_2_661_ZZ_final.r")
